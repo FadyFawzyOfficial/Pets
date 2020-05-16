4e572c5d26a0ba5551b2911e001a1874a74e0c0f
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-05-16 12:13:21 +0200

    1.9 Add UriMatcher to ContentProvider

--- app/src/main/java/com/engineerfadyfawzi/pets/data/PetProvider.java
+++ app/src/main/java/com/engineerfadyfawzi/pets/data/PetProvider.java
@@ -2,6 +2,7 @@ package com.engineerfadyfawzi.pets.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -10,6 +11,46 @@ import android.net.Uri;
  */
 public class PetProvider extends ContentProvider
 {
+    /**
+     * URI matcher code for the content URI for the pets table
+     */
+    private static final int PETS = 100;
+    
+    /**
+     * URI matcher code for the content URI for a single pet in the pets table
+     */
+    private static final int PET_ID = 101;
+    
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this code.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher( UriMatcher.NO_MATCH );
+    
+    // Static initializer. This is run the first time anything is called from this class.
+    static
+    {
+        // The calls to addURI() go here, for all the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+        
+        // COMPLETED: Add 2 content URIs to URI matcher
+        // The content URI of the form "content://com.engineerfadyfawzi.pets/pets" will map to the
+        // integer code {@link #PETS}. This URI is used to provide access to MULTIPLE rows
+        // of the pets table.
+        sUriMatcher.addURI( PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS );
+        
+        // The content URI of the form "content://com.engineerfadyfawzi.pet/pets/#" will map to the
+        // integer code {@link #PET_ID}. This URI is used to provide access to ONE single row
+        // of the pets table.
+        //
+        // In this case, the "#" wild card is used where "#" can be substituted for an integer.
+        // For example, "content://com.engineerfadyfawzi.pets/pets/3" matches, but
+        // "content://com.engineerfadyfawzi.pets/pets" (without a number at the end) doesn't match.
+        sUriMatcher.addURI( PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID );
+    }
+    
     /**
      * Tag for the log messages
      */
@@ -28,9 +69,6 @@ public class PetProvider extends ContentProvider
     @Override
     public boolean onCreate()
     {
-        // COMPLETED: Create and initialize a PetDbHelper object to gain access to the pets database.
-        //  Make sure the variable is a global variable, so it can be referenced from other
-        //  ContentProvider methods.
         mDbHelper = new PetDbHelper( getContext() );
         
         return true;
